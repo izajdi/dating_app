6c54e407345fb1dc9637bbad0c2b0d99ebd6644f
iZajdi <zajda.bartek@gmail.com>
2021-04-29 19:41:07 +0200

    Initial commit

---
+++ src/main/java/com/example/sp/SpProject2021Application.java
@@ -0,0 +1,13 @@
+package com.example.sp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpProject2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpProject2021Application.class, args);
+	}
+
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,55 @@
+package model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name ="first_name")
+    private String firstName;
+
+    @Column(name ="last_name")
+    private String lastName;
+
+    @Column(name ="email")
+    private String email;
+
+    @Column(name ="age")
+    private Integer age;
+
+    public User() {
+
+    }
+
+    public User(String firstName, String lastName, String email, Integer age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
